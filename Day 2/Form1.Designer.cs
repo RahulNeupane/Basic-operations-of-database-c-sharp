ab5626ae27e18f18e9761bf5995f40fea788fb2d
Rahul Neupane <nrahul503@gmail.com>
2021-08-09 14:40:08 +0545

    Initial commit

---
+++ Day 2/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Day_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.std_name = new System.Windows.Forms.TextBox();
+            this.std_address = new System.Windows.Forms.TextBox();
+            this.Std_list = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Add = new System.Windows.Forms.Button();
+            this.Remove = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.stdID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Student_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Action = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.Std_list)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // std_name
+            // 
+            this.std_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.std_name.Location = new System.Drawing.Point(29, 39);
+            this.std_name.Name = "std_name";
+            this.std_name.Size = new System.Drawing.Size(204, 26);
+            this.std_name.TabIndex = 1;
+            // 
+            // std_address
+            // 
+            this.std_address.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.std_address.Location = new System.Drawing.Point(29, 118);
+            this.std_address.Name = "std_address";
+            this.std_address.Size = new System.Drawing.Size(204, 26);
+            this.std_address.TabIndex = 2;
+            // 
+            // Std_list
+            // 
+            this.Std_list.AllowUserToAddRows = false;
+            this.Std_list.AllowUserToDeleteRows = false;
+            this.Std_list.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Std_list.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.stdID,
+            this.SN,
+            this.Student_name,
+            this.Address,
+            this.Action});
+            this.Std_list.Location = new System.Drawing.Point(239, 16);
+            this.Std_list.Name = "Std_list";
+            this.Std_list.ReadOnly = true;
+            this.Std_list.Size = new System.Drawing.Size(626, 332);
+            this.Std_list.TabIndex = 2;
+            this.Std_list.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Std_list_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Address";
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(30, 163);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 40);
+            this.Add.TabIndex = 3;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Remove
+            // 
+            this.Remove.Location = new System.Drawing.Point(158, 163);
+            this.Remove.Name = "Remove";
+            this.Remove.Size = new System.Drawing.Size(75, 40);
+            this.Remove.TabIndex = 4;
+            this.Remove.Text = "Remove";
+            this.Remove.UseVisualStyleBackColor = true;
+            this.Remove.Click += new System.EventHandler(this.Remove_Click);
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(30, 219);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(75, 40);
+            this.Update.TabIndex = 6;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(158, 219);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 40);
+            this.Clear.TabIndex = 7;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_click);
+            // 
+            // stdID
+            // 
+            this.stdID.HeaderText = "ID";
+            this.stdID.Name = "stdID";
+            this.stdID.ReadOnly = true;
+            this.stdID.Visible = false;
+            // 
+            // SN
+            // 
+            this.SN.HeaderText = "SN";
+            this.SN.Name = "SN";
+            this.SN.ReadOnly = true;
+            this.SN.Width = 40;
+            // 
+            // Student_name
+            // 
+            this.Student_name.HeaderText = "Name";
+            this.Student_name.Name = "Student_name";
+            this.Student_name.ReadOnly = true;
+            this.Student_name.Width = 200;
+            // 
+            // Address
+            // 
+            this.Address.HeaderText = "Address";
+            this.Address.Name = "Address";
+            this.Address.ReadOnly = true;
+            this.Address.Width = 200;
+            // 
+            // Action
+            // 
+            this.Action.HeaderText = "Action";
+            this.Action.Name = "Action";
+            this.Action.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 356);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Remove);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Std_list);
+            this.Controls.Add(this.std_address);
+            this.Controls.Add(this.std_name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Std_list)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox std_name;
+        private System.Windows.Forms.TextBox std_address;
+        private System.Windows.Forms.DataGridView Std_list;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Remove;
+        private new System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stdID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Student_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Action;
+    }
+}
+
---
+++ Day 2/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day_2
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=RAHUL\SQLEXPRESS;Initial Catalog=c#;Integrated Security=True"); 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //open sql connection 
+                conn.Open();
+
+                string st_name = std_name.Text;
+                string st_address = std_address.Text;
+
+                //save data to database
+                //string query = "Insert into student_details(name,address) values ("+st_name+","+st_address+")";
+                //SqlCommand cmd = new SqlCommand(query, conn);
+
+
+                string query = "Insert into student_details(name,address) values (@parameter_name,@parameter_address)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@parameter_name", st_name);
+                cmd.Parameters.AddWithValue("@parameter_address", st_address);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Saved successfully");
+
+                std_name.Text = "";
+                std_address.Text = "";
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+            finally
+            {
+                //close sql connection
+                conn.Close();
+            }
+
+            DisplayData();
+
+
+
+            //string name = std_name.Text;
+            //string address = std_address.Text;
+            
+            ////create new rows
+            ////Std_list.Rows.Add();
+
+            //Std_list.Rows.Add(Std_list.Rows.Count + 1, name, address);
+        }
+
+        private void DisplayData()
+        {
+            try
+            {
+                //open sql connection 
+                conn.Open();
+
+
+                string query = "Select * from student_details";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+
+                Std_list.Rows.Clear();
+
+                int sn = 0;
+                foreach(DataRow dataRow in dt.Rows)
+                {
+                    sn++;
+                    Std_list.Rows.Add(dataRow["ID"],sn, dataRow["name"], dataRow["address"],"Edit");
+                    
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+            finally
+            {
+                //close sql connection
+                conn.Close();
+            }
+
+        }
+
+        private void Remove_Click(object sender, EventArgs e)
+        {
+            //for removing single row if any cell is clicked
+            //Std_list.Rows.RemoveAt(Std_list.CurrentRow.Index);
+            try
+            {
+                //open sql connection 
+                conn.Open();
+
+                
+
+                //for removing single row if complete row is selected
+                if (Std_list.SelectedRows.Count > 0)
+                {
+
+                    //for removing single row
+                     //Std_list.Rows.RemoveAt(Std_list.SelectedRows[0].Index);
+
+
+                    //for removing multiple rows
+                    foreach (DataGridViewRow row in Std_list.SelectedRows)
+                    {
+                        
+
+                        int id = Convert.ToInt32(row.Cells["stdID"].Value);
+
+                        //passing parameter method
+                        string query = "Delete from student_details where ID=@parameter_id";
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("@parameter_id", id);
+                        cmd.ExecuteNonQuery();
+                        Std_list.Rows.RemoveAt(row.Index);
+                        MessageBox.Show("Removed sucessfully");
+                    }
+                    
+
+                }
+                else
+                {
+                    MessageBox.Show("Please select row to delete");
+                }
+                //int i = 0;
+                //foreach (DataGridViewRow row in Std_list.Rows)
+                //{
+                //    i++;
+                //    row.Cells["SN"].Value = i;
+                //}
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+            finally
+            {
+                //close sql connection
+                conn.Close();
+            }
+            DisplayData();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayData();
+        }
+        int student_id;
+        private void Std_list_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.ColumnIndex == Std_list.Columns["Action"].Index)
+            {
+                string student_name = Std_list.CurrentRow.Cells["Student_name"].Value.ToString();
+                student_id = Convert.ToInt32(Std_list.CurrentRow.Cells["SN"].Value.ToString());
+                string student_address = Std_list.CurrentRow.Cells["Address"].Value.ToString();
+
+                MessageBox.Show("Id: " + student_id + "\n Name: " + student_name);
+
+                std_name.Text = student_name;
+                std_address.Text = student_address;
+            }
+        }
+
+        private void Update_click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (student_id != 0)
+                {//open sql connection 
+                    conn.Open();
+
+                    string st_name = std_name.Text;
+                    string st_address = std_address.Text;
+
+                    //save data to database
+                    //string query = "Insert into student_details(name,address) values ("+st_name+","+st_address+")";
+                    //SqlCommand cmd = new SqlCommand(query, conn);
+
+
+                    string query = "Update student_details set name=@parameter_name ,address=@parameter_address where id = @parameter_id";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@parameter_name", st_name);
+                    cmd.Parameters.AddWithValue("@parameter_address", st_address);
+                    cmd.Parameters.AddWithValue("@parameter_id", student_id);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Saved successfully");
+
+                    std_name.Text = "";
+                    std_address.Text = "";
+                    student_id = 0;
+                }
+                else
+                {
+                    MessageBox.Show("please edit data before update");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+            finally
+            {
+                //close sql connection
+                conn.Close();
+            }
+
+            DisplayData();
+        }
+
+        private void Clear_click(object sender, EventArgs e)
+        {
+            std_name.Text = "";
+            std_address.Text = "";
+            student_id = 0;
+        }
+    }
+}
